03435b5337a16a0bb856097c8280ad177af5076c
abhi87000 <110568202+abhi87000@users.noreply.github.com>
2023-04-08 22:50:39 +0530

    Add files via upload

---
+++ assets/js/common.js
@@ -0,0 +1,27 @@
+
+function createEnq()
+    {
+	                                       $('#thn01').hide();
+                    $('#5').show();
+                                        $('#4').show();
+                                        $('#3').show();
+                    $('#2').show();
+                                        $('#1').show();
+		$('#ths').show();
+
+      $('#enquer01').show(); 
+    }
+$('.close').click(function(){
+      $('#enquer01').hide();
+});
+/*document.getElementById("myButton").click = function () {
+        location.href="kerala.php";
+    };
+*/
+$("#myButton").click(function(){
+window.location.href="kerala.php";
+})
+
+$("#myButton1").click(function(){
+window.location.href="bhutan.php";
+})
